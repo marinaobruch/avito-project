cc72fc86ad7424c8143e5f34cbbd3d551957e708
Marina Obruch <marina_obruch@mail.ru>
2024-01-04 21:07:13 +0500

    token | repeated request for token, there are some spots

--- src/store/services/ads-service.tsx
+++ src/store/services/ads-service.tsx
@@ -7,89 +7,98 @@ import {
 } from '@reduxjs/toolkit/query/react'
 import { ICommemtRequest, IDeleteImgRequest, IPatchAd, IPostAdv, IPostComment, IPostImgInAdv, IToken, IUserLogin, IUserPatch, IUserReg, IUserRequest } from "interface/api-interface";
 import { IRequestAds } from "interface/api-interface";
-// import { clearTokens, setAccessToken, setRefreshToken } from "..";
+import { clearTokens, setAccessToken, setRefreshToken } from "..";
 
-// const baseQueryWithReauth: BaseQueryFn<
-//   string | FetchArgs,
-//   unknown,
-//   FetchBaseQueryError
-//     > = async (args, api, extraOptions) => {
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+    > = async (args, api, extraOptions) => {
 
-//     const baseQuery = fetchBaseQuery({
-//         baseUrl: 'http://localhost:8090',
-//         prepareHeaders: (headers) => {
-//             const token = localStorage.getItem('access_token')
-//             console.debug('Использую токен из стора', { token })
-//             if (token) {
-//                 headers.set('authorization', `Bearer ${token}`)
-//             }
-//             return headers
-//         },
-//     })
+    const baseQuery = fetchBaseQuery({
+        baseUrl: 'http://localhost:8090',
+        prepareHeaders: (headers) => {
+            const token = localStorage.getItem('access_token')
+            console.debug('Использую токен из стора', { token })
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`)
+            }
+            return headers
+        },
+    })
 
-//     const result = await baseQuery(args, api, extraOptions)
+    const result = await baseQuery(args, api, extraOptions)
+    console.debug('Результат первого запроса', { result })
 
-//     if (result?.error?.status !== 401) {
-//       return result
-//     }
+    if (result?.error?.status !== 401) {
+      return result
+    }
   
-//     const forceLogout = () => {
-//       api.dispatch(clearTokens())
-//       window.location.replace('/login')
-//     }
-//     const refresh = localStorage.getItem('refresh_token')
-//     if (!refresh) {
-//       return forceLogout()
-//     }
-  
-//     const refreshResult = await baseQuery(
-//       {
-//         url: 'auth/login',
-//         method: 'PUT',
-//         body: {
-//           access_token: localStorage.getItem('access_token'),
-//           refresh_token: localStorage.getItem('refresh_token'),
-//         },
-//       },
-//       api,
-//       extraOptions,
-//     )
-  
-//     if (!refreshResult.data.access_token) {
-//       return forceLogout()
-//     }
-  
-//     api.dispatch(setAccessToken( refreshResult.data.access_token ))
-//     api.dispatch(setRefreshToken( refreshResult.data.refresh_token ))
+    const forceLogout = () => {
+    //   api.dispatch(clearTokens())
+      console.debug('Принудительная авторизация!')
+      localStorage.removeItem('refresh_token');
+      localStorage.removeItem('access_token');
+      window.location.replace('/login')
+    }
+    const refresh = localStorage.getItem('refresh_token')
+    console.debug('Данные пользователя в сторе', { refresh })
 
-//     localStorage.setItem("access_token", refreshResult.data.access_token)
-//     localStorage.setItem("refresh_token", refreshResult.data.refresh_token)
+    if (!refresh) {
+      return forceLogout()
+    }
   
-//     const retryResult = await baseQuery(args, api, extraOptions)
+    const refreshResult = await baseQuery(
+      {
+        url: 'auth/login',
+        method: 'PUT',
+        body: {
+          access_token: localStorage.getItem('access_token'),
+          refresh_token: localStorage.getItem('refresh_token'),
+        },
+      },
+      api,
+      extraOptions,
+    )
   
-//     if (retryResult?.error?.status === 401) {
-//       return forceLogout()
-//     }
+    if (!refreshResult.data.access_token) {
+      return forceLogout()
+    }
 
-//     return retryResult
-//   }
+    console.debug('Результат запроса на обновление токена', { refreshResult })
+  
+    // api.dispatch(setAccessToken( refreshResult.data.access_token ))
+    // api.dispatch(setRefreshToken( refreshResult.data.refresh_token ))
+
+    localStorage.setItem("access_token", refreshResult.data.access_token)
+    localStorage.setItem("refresh_token", refreshResult.data.refresh_token)
+  
+    const retryResult = await baseQuery(args, api, extraOptions)
+  
+    if (retryResult?.error?.status === 401) {
+      return forceLogout()
+    }
+    
+    console.debug('Повторный запрос завершился успешно')
+    return retryResult
+  }
 
 
 export const avitoApi = createApi({
     reducerPath: 'avitoApi',
     tagTypes: ['Users', 'Comments', 'Ads'],
-    // baseQuery: baseQueryWithReauth,
+    baseQuery: baseQueryWithReauth,
 
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8090',
-        prepareHeaders: (headers) => {
-            const token = localStorage.getItem('access_token')
-            if (token) {
-              headers.set('authorization', `Bearer ${token}`)
-            }
-            return headers;
-          },
-    }),
+    // baseQuery: fetchBaseQuery({
+    //     baseUrl: 'http://localhost:8090',
+    //     prepareHeaders: (headers) => {
+    //         const token = localStorage.getItem('access_token')
+    //         if (token) {
+    //           headers.set('authorization', `Bearer ${token}`)
+    //         }
+    //         return headers;
+    //       },
+    // }),
 
     endpoints: (build) => ({
     //ADS
